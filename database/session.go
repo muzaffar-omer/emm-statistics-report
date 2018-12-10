69e7bbb6ce85b0f3874081d91536a98b4ac6008e
muzaffar-omer <muzaffar.omer@gmail.com>
2018-12-10 18:18:25 +0300

    Added 'steps' section

---
+++ database/session.go
@@ -0,0 +1,78 @@
+package database
+
+import (
+	config "emm-statistics-report/configuration"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+var sessionsPool []Session
+
+type Session struct {
+	logicalServer *config.LogicalServer
+	Db            *sqlx.DB
+}
+
+// Checks whether there is already an existing working session
+// If it finds one, returns pointer to the session, otherwise, returns nil
+func isSessionExists(logicalServer *config.LogicalServer) *Session {
+
+	for _, existingSession := range sessionsPool {
+		if existingSession.logicalServer.Equals(logicalServer) {
+			return &existingSession
+		}
+	}
+
+	return nil
+}
+
+// Opens a session to a logical server database and adds the
+// session to the pool
+func CreateSession(ls *config.LogicalServer) *Session {
+
+	var newSession *Session
+
+	if newSession = isSessionExists(ls); newSession != nil {
+		return newSession
+	}
+
+	connStr := fmt.Sprintf("user=%s dbname=%s password=%s port=%s host=%s sslmode=disable", ls.Username, ls.Database, ls.Password, ls.Port, ls.Ip)
+
+	db, err := sqlx.Open("postgres", connStr)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"logical_server": ls.Name,
+			"database":       ls.Database,
+			"ip":             ls.Ip,
+			"port":           ls.Port,
+			"error":          err,
+		}).Error("Opening session")
+
+		return nil
+
+	} else {
+		newSession = &Session{logicalServer: ls, Db: db}
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"logical_server": ls.Name,
+			"database":       ls.Database,
+			"ip":             ls.Ip,
+			"port":           ls.Port,
+			"error":          err,
+		}).Error("Ping database")
+
+		return nil
+	}
+
+	sessionsPool = append(sessionsPool, *newSession)
+
+	return newSession
+}
